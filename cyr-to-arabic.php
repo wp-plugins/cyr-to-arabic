dc2b1ad7db3ba9ccd03e5ef9cdd9173065515c38
Janibek7 <Janibek7@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-05 13:54:11 +0000

    1. Modify the function code. 2. Fixes minor bugs.
    git-svn-id: https://plugins.svn.wordpress.org/cyr-to-arabic/trunk@927143 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cyr-to-arabic.php
+++ cyr-to-arabic.php
@@ -5,7 +5,7 @@ Plugin Name: cyr-to-arabic
 Plugin URI: http://www.janibek.kz/cyr-to-arabic
 Description: Convert text from Kazakh Cyrillic to Arabic script in posts. 2014.1.1, Altai, China.
 Author: Janibek Sheryazdan
-Version: 0.2
+Version: 0.3
 Author URI: http://www.janibek.kz
 */
 
